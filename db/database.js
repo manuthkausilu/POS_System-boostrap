25317642bbd47446bcec36170b1d5d3925591129
ASUS <manuthkausilu20031018@gmail.com>
2024-12-21 09:50:42 +0530

    1st commit

--- db/database.js
+++ db/database.js
@@ -1,3 +1,11 @@
-export let CustomerDB = [];
-export let ItemDB = [];
-export let OrderDB = [];
+//customer array
+export let customer_array = [];
+
+//item array
+export let item_array = [];
+
+//cart array
+export let cart = [];
+
+//order array
+export let order_array = [];
